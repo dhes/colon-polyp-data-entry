c7e02c7cfcae7f207cf46ac273277475ccfba6dc
Dan Heslinga <dan@heslinga.us>
2022-07-18 09:50:23 -1000

    first write

--- src/main.js
+++ src/main.js
@@ -1,2 +1,11 @@
-var msg = 'Hello World';
-console.log(msg);
+const yaml = require('js-yaml');
+const fs = require('fs');
+const { Z_FIXED } = require('zlib');
+a = {
+    "resourceType": "Procedure",
+    "id": "96582371-740e-4fbf-9a7b-7a1450472b76",
+    "performedDateTime": "2021-02-21",
+    "code": "SNOMEDCT#736719000 Repair of left inguinal hernia (procedure)",
+};
+console.log(a);
+fs.writeFileSync('procedure.yml',yaml.dump(a));
