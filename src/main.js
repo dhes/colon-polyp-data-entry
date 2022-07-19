d874e298b2c9b6db02adcb237ad4e44a51c524e2
Dan Heslinga <dan@heslinga.us>
2022-07-18 14:54:58 -1000

    generates a uuidv4

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
+// const uuidv4 = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 // a = {
 //     "resourceType": "Procedure",
 //     "id": "96582371-740e-4fbf-9a7b-7a1450472b76",
@@ -9,4 +11,4 @@ const fs = require('fs');
 // console.log(a);
 // fs.writeFileSync('procedure.yml',yaml.dump(a));
 const doc = yaml.load(fs.readFileSync('src/report_template.yml','utf8'));
-console.log(doc);
+console.log(uuidv4());
