e3ffb2d9b89438933593db6f5f40d7751ba3889c
Dan Heslinga <dan@heslinga.us>
2022-07-18 14:43:15 -1000

    add uuid to package.json; read yaml file;

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 const yaml = require('js-yaml');
 const fs = require('fs');
-// const { Z_FIXED } = require('zlib');
-a = {
-    "resourceType": "Procedure",
-    "id": "96582371-740e-4fbf-9a7b-7a1450472b76",
-    "performedDateTime": "2021-02-21",
-    "code": "SNOMEDCT#736719000 Repair of left inguinal hernia (procedure)",
-};
-console.log(a);
-fs.writeFileSync('procedure.yml',yaml.dump(a));
+// a = {
+//     "resourceType": "Procedure",
+//     "id": "96582371-740e-4fbf-9a7b-7a1450472b76",
+//     "performedDateTime": "2021-02-21",
+//     "code": "SNOMEDCT#736719000 Repair of left inguinal hernia (procedure)",
+// };
+// console.log(a);
+// fs.writeFileSync('procedure.yml',yaml.dump(a));
+const doc = yaml.load(fs.readFileSync('src/report_template.yml','utf8'));
+console.log(doc);
