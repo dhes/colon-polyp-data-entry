744b0fe975f2ef764d4432c3cbbf1be6bcf4010b
Dan Heslinga <drdan@heslingamd.com>
2022-07-25 16:06:36 -1000

    short sample of yaml type condition list and string-type condition list

--- src/compact_summary.js
+++ src/compact_summary.js
@@ -1,10 +1,33 @@
 // reformat a yml test file to make it a little more human readable
-const yaml = require('js-yaml');
-const fs = require('fs');
-const testFile = yaml.load(fs.readFileSync('Medicare_Male.yml'));
-// console.log(testFile);
-// console.log(testFile[0].resourceType);
-const conditions = testFile.filter(resource => resource.resourceType=='Condition');
-console.log(conditions);
-
-
+const yaml = require("js-yaml");
+const fs = require("fs");
+const testFile = yaml.load(fs.readFileSync("Medicare_Male.yml", "utf8"));
+let Conditions = testFile.filter(
+  (resource) => resource.resourceType == "Condition"
+);
+let Problems = structuredClone(Conditions);
+for (const r of Conditions) {
+  delete r.resourceType;
+  const indexOfHash = (r['code']).indexOf('#');
+  const indexOfSpace = (r['code']).indexOf(' ');
+  r['name'] = r['code'].substring(indexOfSpace + 1);
+  r['code'] = r['code'].substring(0, indexOfHash) + " " + r['code'].substring(indexOfHash + 1, indexOfSpace);
+  r['onset'] =  r['onsetDateTime'];
+  delete r['onsetDateTime'];
+  r['onset'] = (r['onset']).substring(0,10);
+}
+Conditions = {Conditions};
+fs.writeFileSync("compactSummary.yml", yaml.dump(Conditions, { noRefs: true}));
+// or...
+let condition = "Conditions:\n  ";
+for (const r of Problems) {
+    const indexOfHash = (r['code']).indexOf('#');
+    const indexOfSpace = (r['code']).indexOf(' ');
+    // r['name'] = r['code'].substring(indexOfSpace + 1);
+    const name = r['code'].substring(indexOfSpace + 1);
+    // const codeSystem = ;
+    const code = r['code'].substring(0, indexOfHash) + " " + r['code'].substring(indexOfHash + 1, indexOfSpace)
+    condition += "- " + name + " (" + code + ")\n  ";
+}
+console.log(condition);
+fs.writeFileSync("condition.yml", yaml.dump(condition, { noRefs: true}));
